988231572535af48849ccd3fb9afae94013d4537
subhranshudas <subhranshu.opt@gmail.com>
2024-12-04 15:27:12 +0530

    feat: add implementation for empty string test case

--- src/utils/string-calculator.ts
+++ src/utils/string-calculator.ts
@@ -1,4 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function add(_numbers: string): number {
-  return 1; // Placeholder implementation
+  if (_numbers === "") {
+    return 0;
+  }
+  return 100;
 }
