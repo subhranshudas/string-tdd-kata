0366dc147b89be838dc5f13b3634399549c7746f
subhranshudas <subhranshu.opt@gmail.com>
2024-12-04 15:47:18 +0530

    test: add a failing test for newline as delimiter

--- __tests__/string-calculator.test.ts
+++ __tests__/string-calculator.test.ts
@@ -18,4 +18,10 @@ describe("add", () => {
     expect(add("1,2,3,4")).toBe(10);
     expect(add("10,20,30,40")).toBe(100);
   });
+
+  it("should handle newline as delimiter", () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("1,2\n3")).toBe(6);
+    expect(add("1\n2\n3")).toBe(6);
+  });
 });
