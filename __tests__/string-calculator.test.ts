cee1bd0cffb829023d15821b0130f54c259d5cdf
subhranshudas <subhranshu.opt@gmail.com>
2024-12-04 15:45:42 +0530

    test: add test for multiple numbers

--- __tests__/string-calculator.test.ts
+++ __tests__/string-calculator.test.ts
@@ -12,4 +12,10 @@ describe("add", () => {
   it("should return sum for two numbers", () => {
     expect(add("1,2")).toBe(3);
   });
+
+  it("should return sum for multiple numbers", () => {
+    expect(add("1,2,3")).toBe(6);
+    expect(add("1,2,3,4")).toBe(10);
+    expect(add("10,20,30,40")).toBe(100);
+  });
 });
