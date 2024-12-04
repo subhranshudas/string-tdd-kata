3d5d32b5a77370520ae50ada876c45e06704f711
subhranshudas <subhranshu.opt@gmail.com>
2024-12-04 15:31:40 +0530

    test: add a failing test for the single number input

--- __tests__/string-calculator.test.ts
+++ __tests__/string-calculator.test.ts
@@ -4,4 +4,8 @@ describe("add", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  it("should return the number for single number string", () => {
+    expect(add("1")).toBe(1);
+  });
 });
